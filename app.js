f06ead112249fdbf10b95ea6941b7c2804c90842
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-10 15:45:48 +0800

    implementing connect modules

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var http = require('http')
+// Before using http module
+// var http = require('http')
+//
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   })
+//   res.end('Hello World')
+// }).listen(3000)
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.end('Hello World')
-}).listen(3000)
+// After use connect module
+var connect = require('connect')
+var app = connect()
 
-console.log('Server running at http://localhost:3000/')
+app.listen(4000)
+
+console.log('Server running at http://localhost:4000/')
