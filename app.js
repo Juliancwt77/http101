22411327ab92757583e83b2ca6a3255a27e2cbaf
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-11 09:21:23 +0800

    latest update on connect module

--- app.js
+++ app.js
@@ -10,8 +10,45 @@
 
 // After use connect module
 var connect = require('connect')
+console.log('connect is a ' + typeof connect)
 var app = connect()
 
-app.listen(4000)
+app.use(function (req, res, next) {
+  console.log('new req method is: ' + req.method, 'new req url is: ' + req.url)
+  next()
+})
 
+// // use set timeout
+// app.use(function (req, res, next) {
+//   console.log('new req method is: ' + req.method, 'new req url is: ' + req.url)
+//   next()
+// })
+//
+// // use for loop 5000 times
+// app.use(function (req, res, next) {
+//   console.log('new req method is: ' + req.method, 'new req url is: ' + req.url)
+//   next()
+// })
+
+// response once
+var home = function (req, res) {
+  res.end('homepage')
+}
+
+var helloWorld = function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
+}
+
+// response twice
+var goodbyeWorld = function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Bye World')
+}
+
+app.use('/hello', helloWorld)
+app.use('/goodbye', goodbyeWorld)
+app.use('/', home)
+
+app.listen(4000)
 console.log('Server running at http://localhost:4000/')
